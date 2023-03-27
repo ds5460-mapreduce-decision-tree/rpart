f32ef191f7cfa356c62cb76f568060f52c8d8c45
irrationomical <kc@kcbarrett.org>
2023-03-27 15:16:09 -0500

    fixed typo in dataset.py

--- rpart/dataset.py
+++ rpart/dataset.py
@@ -4,6 +4,6 @@ from importlib import resources
 import pandas as pd
 
 
-def load_cencus(**kwargs) -> pd.DataFrame:
+def load_census(**kwargs) -> pd.DataFrame:
     with resources.open_text("data", "adult.csv") as f:
         return pd.read_csv(f, **kwargs)
