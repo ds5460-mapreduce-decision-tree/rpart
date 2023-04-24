95d2cf13ac8e99937ec4067520b36587d9853f01
Qiushi Yan <qiushi.yann@gmail.com>
2023-04-23 22:27:06 -0500

    update

--- rpart/DecisionTreeClassifier.py
+++ rpart/DecisionTreeClassifier.py
@@ -1,47 +1,46 @@
 import numpy as np
 import pandas as pd
 from collections import Counter
-from scipy.stats import chi2_contingency
-
-
-class Node:
-    def __init__(
-        self,
-        feature=None,
-        threshold=None,
-        left=None,
-        right=None,
-        depth=None,
-        *,
-        value=None,
-    ):
-        self.feature = feature
-        self.threshold = threshold
-        self.left = left
-        self.right = right
-        self.value = value
-        self.depth = depth
-
-    def is_leaf_node(self):
-        return self.value is not None
+from rpart.utils import chimerge, entropy, gini, reduceByKey
+from rpart.Node import Node
+from rpart.utils import draw_tree
+import multiprocessing as mp
 
 
 class DecisionTreeClassifier:
     def __init__(
         self,
-        max_depth=None,
-        metric="gini",
-        split_method=None,
-        chimerge_threshold=0.05,
-        chimerge_max_intervals=None,
+        max_depth: int = None,
+        metric: str = "gini",
+        split_method: str = None,
+        chimerge_threshold: float = 0.05,
+        chimerge_max_intervals: int = None,
+        n_workers: int = 5,
     ):
         self.max_depth = max_depth
         self.metric = metric
         self.split_method = split_method
         self.chimerge_threshold = chimerge_threshold
         self.chimerge_max_intervals = chimerge_max_intervals
+        self.n_workers = n_workers
 
     def fit(self, X, y):
+        self._prepare_fit(X, y)
+        self.root = self._grow_tree(X, y)
+
+    def fit_mapreduce(self, X, y):
+        self._prepare_fit(X, y)
+        self.root = self._grow_tree_mapreduce(X, y)
+
+    def predict(self, X):
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X, columns=self.feature_names)
+        return np.array([self._predict(x) for _, x in X.iterrows()])
+
+    def draw(self):
+        return draw_tree(self.root, self.feature_names)
+
+    def _prepare_fit(self, X, y):
         if isinstance(X, np.ndarray):
             X = pd.DataFrame(X)
             self.feature_names = [f"Feature {i}" for i in range(X.shape[1])]
@@ -50,51 +49,64 @@ class DecisionTreeClassifier:
         self.n_classes = len(set(y))
         self.n_features = X.shape[1]
         self.feature_names = X.columns
-        self.root = self._grow_tree(X, y)
-
-    def predict(self, X):
-        if isinstance(X, np.ndarray):
-            X = pd.DataFrame(X, columns=self.feature_names)
-        return np.array([self._predict(x) for _, x in X.iterrows()])
 
     def _grow_tree(self, X, y, depth=0):
+        if self._should_stop(X, y, depth):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        else:
+            feature, threshold = self._best_split(X, y)
+            if feature is None:
+                leaf_value = self._most_common_label(y)
+                return Node(value=leaf_value)
+
+            mask = X.iloc[:, feature] <= threshold
+            left = self._grow_tree(X[mask], y[mask], depth + 1)
+            right = self._grow_tree(X[~mask], y[~mask], depth + 1)
+            return Node(feature, threshold, left, right)
+
+    def _should_stop(self, X, y, depth):
         n_samples, n_features = X.shape
         n_labels = len(np.unique(y))
 
-        # Stopping conditions
         if (
             (self.max_depth is not None and depth >= self.max_depth)
             or n_labels == 1
             or n_samples < 2
             or np.all(X == X.iloc[0].values)
         ):
+            return True
+        return False
+
+    def _grow_tree_mapreduce(self, X, y, depth=0, mask=None):
+        if mask is not None:
+            X = X[mask]
+            y = y[mask]
+
+        if self._should_stop(X, y, depth):
             leaf_value = self._most_common_label(y)
             return Node(value=leaf_value)
 
-        feature, threshold = self._best_split(X, y)
-        if feature is None:
-            leaf_value = self._most_common_label(y)
-            return Node(value=leaf_value)
+        else:
+            partitions = self._partition_data(X, y)
+            feature, threshold = self._best_split_mapreduce(partitions)
+            if feature is None:
+                leaf_value = self._most_common_label(y)
+                return Node(value=leaf_value)
 
-        mask = X.iloc[:, feature] <= threshold
-        left = self._grow_tree(X[mask], y[mask], depth + 1)
-        right = self._grow_tree(X[~mask], y[~mask], depth + 1)
-        return Node(feature, threshold, left, right)
+            mask = X.iloc[:, feature] <= threshold
+            left = self._grow_tree_mapreduce(X, y, depth + 1, mask)
+            right = self._grow_tree_mapreduce(X, y, depth + 1, ~mask)
+            return Node(feature, threshold, left, right)
 
     def _best_split(self, X, y):
         best_split = None
         min_score = float("inf")
-        for feature in range(self.n_features):
+        feature_thresholds = self._get_thresholds(X, y)
+        for item in feature_thresholds:
+            feature, thresholds = item
             x = X.iloc[:, feature]
-            unique_values = np.unique(x)
-
-            # If the feature is numeric, use midpoints; otherwise, use all unique values
-            if np.issubdtype(x.dtype, np.number):
-                sorted_values = np.sort(unique_values)
-                thresholds = (sorted_values[:-1] + sorted_values[1:]) / 2
-            else:
-                thresholds = unique_values
-
             for threshold in thresholds:
                 mask = x <= threshold
                 left, right = y[mask], y[~mask]
@@ -104,41 +116,49 @@ class DecisionTreeClassifier:
                     min_score = score
         return best_split
 
-    def _chimerge(self, x, y):
-        df = pd.DataFrame({"x": x, "y": y}).sort_values("x")
-        intervals = [(row["x"], row["x"]) for _, row in df.iterrows()]
+    def _best_split_mapreduce(self, partitions):
+        with mp.Pool(self.n_workers) as pool:
+            feature_threshold_scores = pool.map_async(self._best_split_mapper, partitions)
 
-        while True:
-            chi_values = []
-            for i in range(len(intervals) - 1):
-                interval_1, interval_2 = intervals[i], intervals[i + 1]
-                mask_1 = (df["x"] >= interval_1[0]) & (df["x"] <= interval_1[1])
-                mask_2 = (df["x"] >= interval_2[0]) & (df["x"] <= interval_2[1])
-                crosstab = pd.crosstab(df[mask_1 | mask_2]["y"], mask_1[mask_1 | mask_2])
-                chi_value = chi2_contingency(crosstab, correction=False)[0]
-                chi_values.append(chi_value)
+        feature_threshold_scores = [
+            result for result in feature_threshold_scores.get() if result is not None
+        ]
+        feature, threshold = self._best_split_reducer(feature_threshold_scores)
+        return feature, threshold
 
-            min_chi_value = min(chi_values)
-            if min_chi_value > self.chimerge_threshold or (
-                self.chimerge_max_intervals
-                and len(intervals) <= self.chimerge_max_intervals
-            ):
-                break
+    def _best_split_mapper(self, partition):
+        X, y = partition
+        feature_thresholds = self._get_thresholds(X, y)
+        feature_threshold_scores = []
+        for item in feature_thresholds:
+            feature, thresholds = item
+            x = X.iloc[:, feature]
+            for threshold in thresholds:
+                mask = x <= threshold
+                left, right = y[mask], y[~mask]
+                score = self._split_score_mapreduce(left, right)
+                feature_threshold_scores.append(((feature, threshold), score))
+        return feature_threshold_scores
 
-            min_chi_index = chi_values.index(min_chi_value)
-            intervals[min_chi_index] = (
-                intervals[min_chi_index][0],
-                intervals[min_chi_index + 1][1],
-            )
-            del intervals[min_chi_index + 1]
-
-        return intervals
+    def _best_split_reducer(self, feature_threshold_scores):
+        sorted_scores = sorted(
+            [
+                (feature_threshold, score)
+                for feature_threshold, score in reduceByKey(
+                    lambda x, y: x + y, feature_threshold_scores
+                )
+            ],
+            key=lambda x: x[1],
+            reverse=True,
+        )
+        feature, threshold = sorted_scores[0][0]
+        return feature, threshold
 
     def _split_score(self, left, right):
         if self.metric == "gini":
-            score_func = self._gini
+            score_func = gini
         elif self.metric == "entropy":
-            score_func = self._entropy
+            score_func = entropy
         else:
             raise ValueError(
                 "Invalid metric. Supported metrics are 'gini' and 'entropy'."
@@ -149,15 +169,34 @@ class DecisionTreeClassifier:
         score = p_left * score_func(left) + p_right * score_func(right)
         return score
 
-    def _gini(self, y):
-        _, counts = np.unique(y, return_counts=True)
-        probabilities = counts / len(y)
-        return 1 - np.sum(np.square(probabilities))
+    def _split_score_mapreduce(self, left, right):
+        p_left = len(left) / (len(left) + len(right))
+        p_right = len(right) / (len(left) + len(right))
+        score = p_left * gini(left) + p_right * gini(right)
 
-    def _entropy(self, y):
-        _, counts = np.unique(y, return_counts=True)
-        probabilities = counts / len(y)
-        return -np.sum(probabilities * np.log2(probabilities))
+        return score
+
+    def _get_thresholds(self, X, y):
+        feature_thresholds = []
+        for feature in range(self.n_features):
+            x = X.iloc[:, feature]
+            unique_values = np.unique(x)
+
+            # If the feature is numeric, use midpoints; otherwise, use all unique values
+            if np.issubdtype(x.dtype, np.number):
+                if self.split_method == "chimerge":
+                    thresholds = chimerge(
+                        x, y, self.chimerge_threshold, self.chimerge_max_intervals
+                    )
+                else:
+                    sorted_values = np.sort(unique_values)
+                    thresholds = (sorted_values[:-1] + sorted_values[1:]) / 2
+
+            else:
+                thresholds = unique_values
+
+            feature_thresholds.append((feature, thresholds))
+        return feature_thresholds
 
     def _most_common_label(self, y):
         return Counter(y).most_common(1)[0][0]
@@ -172,35 +211,19 @@ class DecisionTreeClassifier:
                 node = node.right
         return node.value
 
-    def draw(self):
-        from graphviz import Digraph
+    def _partition_data(self, X, y):
+        partitions = []
+        partition_size = len(X) // self.n_workers
 
-        def draw_node(node, parent=None, edge_label=None):
-            nonlocal node_counter, dot
-            if node.is_leaf_node():
-                label = f"Leaf: {node.value}"
+        for i in range(self.n_workers):
+            if i == self.n_workers - 1:
+                # For the last partition, include the remaining rows
+                X_partition = X.iloc[i * partition_size :]
+                y_partition = y.iloc[i * partition_size :]
             else:
-                feature_name = self.feature_names[node.feature]
-                if isinstance(node.threshold, (int, float)):
-                    threshold_str = f"{node.threshold:.2f}"
-                else:
-                    threshold_str = str(node.threshold)
+                X_partition = X.iloc[i * partition_size : (i + 1) * partition_size]
+                y_partition = y.iloc[i * partition_size : (i + 1) * partition_size]
 
-                label = f"Feature {feature_name} <= {threshold_str}"
+            partitions.append((X_partition, y_partition))
 
-            node_id = f"node{node_counter}"
-            dot.node(node_id, label)
-
-            if parent is not None:
-                dot.edge(parent, node_id, label=edge_label)
-
-            node_counter += 1
-
-            if not node.is_leaf_node():
-                draw_node(node.left, node_id, "True")
-                draw_node(node.right, node_id, "False")
-
-        dot = Digraph()
-        node_counter = 0
-        draw_node(self.root)
-        return dot
+        return partitions
--- rpart/entropy.py
+++ rpart/entropy.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import itertools
 
+
 # Calculate Gini impurity for a given target variable (y)
 def gini_impurity(y):
     # Check if input is a Pandas Series
@@ -14,6 +15,7 @@ def gini_impurity(y):
         return gini
     else:
         raise ("Object must be a Pandas Series.")
+
 
 # Calculate entropy for a given target variable (y)
 def entropy(y):
@@ -27,6 +29,7 @@ def entropy(y):
     else:
         raise ("Object must be a Pandas Series.")
 
+
 # Calculate variance for a given target variable (y)
 def variance(y):
     # Check if there is only one element
@@ -35,6 +38,7 @@ def variance(y):
     else:
         # Calculate variance
         return y.var()
+
 
 # Calculate information gain for a given split (mask) and target variable (y)
 def information_gain(y, mask, func=entropy):
@@ -59,27 +63,29 @@ def information_gain(y, mask, func=entropy):
             )
     return information_gain
 
+
 # Generate all possible combinations of categories for a categorical variable (a)
 # Not sure we need this, need to replace the code in max_information_gain_split if dropped
 def cat_options(a):
     a = a.unique()
     options = []
-    for L in range(0, len(a)+1):
+    for L in range(0, len(a) + 1):
         for subset in itertools.combinations(a, L):
             subset = list(subset)
             options.append(subset)
     return options[1:-1]
 
+
 # Find the best split for a given feature (x) and target variable (y) based on information gain
 def max_information_gain_split(x, y, func=entropy):
     split_value = []
-    ig = [] 
+    ig = []
     # Determine if the feature is numeric or categorical
-    numeric_variable = True if x.dtypes != 'O' else False
+    numeric_variable = True if x.dtypes != "O" else False
     # Create options according to variable type
     if numeric_variable:
         options = x.sort_values().unique()[1:]
-    else: 
+    else:
         options = cat_options(x)
     # Calculate information gain for all values
     for val in options:
@@ -90,13 +96,13 @@ def max_information_gain_split(x, y, func=entropy):
         split_value.append(val)
     # Check if there are more than 1 results if not, return False
     if len(ig) == 0:
-        return( None, None, None, False)
+        return (None, None, None, False)
     else:
         # Get results with highest information gain (IG)
         best_ig = max(ig)
         best_ig_index = ig.index(best_ig)
         best_split = split_value[best_ig_index]
-        return(best_ig, best_split, numeric_variable, True)
+        return (best_ig, best_split, numeric_variable, True)
 
 
 # Find the best split for all features in the dataset (df) and target variable (y)
@@ -115,7 +121,8 @@ def best_split(df, y, func=entropy):
     best_ig_index = best_ig.index(best_ig)
     best_split = best_split[best_ig_index]
     numeric_variable = numeric_variable[best_ig_index]
-    return(best_ig, best_split, numeric_variable)
+    return (best_ig, best_split, numeric_variable)
+
 
 # # Make a binary split of the data based on the variable, value, and type (numeric or categorical)
 # def make_split(variable, value, data, is_numeric):
@@ -127,10 +134,20 @@ def best_split(df, y, func=entropy):
 #         data_2 = data[(data[variable].isin(value)) == False]
 #     return(data_1, data_2)
 
+
 # Recursively build a decision tree based on the input data, target variable, and optional parameters
-def make_tree(data, target, func=entropy, max_depth=None, min_samples_split=2, depth=0, target_factor=True):
+def make_tree(
+    data,
+    target,
+    func=entropy,
+    max_depth=None,
+    min_samples_split=2,
+    depth=0,
+    target_factor=True,
+):
     # placeholder for the decision tree
-    return 
+    return
+
 
 # Make predictions for a test dataset using the decision tree
 def predict(tree, test_data):
@@ -143,6 +160,7 @@ def predict(tree, test_data):
         predictions.append(prediction)
         # Return the list of predictions
     return predictions
+
 
 # Recursively traverse the decision tree to obtain a prediction for a single instance (row)
 def traverse_tree(tree, row):
